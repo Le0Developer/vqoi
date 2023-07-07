3294f1a041e345105d248bb057a909b58ed850b4
Garo <garo@vp.pl>
2023-07-07 13:36:18 +0200

    fix example code

--- example/test_qoi.v
+++ example/test_qoi.v
@@ -15,9 +15,9 @@ fn main() {
 	}
 }
 
-fn try_file(filename string) ? {
-	data := (os.read_file(filename) ?).bytes()
-	image := vqoi.decode(data) ?
+fn try_file(filename string) ! {
+	data := (os.read_file(filename) or { return err }).bytes()
+	image := vqoi.decode(data) or { return err }
 	encoded := vqoi.encode(image)
 	if data != encoded {
 		return error('does not match')
